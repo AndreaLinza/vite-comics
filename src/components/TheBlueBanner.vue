d09885b31ee4f151d868e0c3b882b1b6b5e82cb7
AndreaLinza <andrealinza@gmail.com>
2023-08-25 10:17:29 +0200

    test

--- src/components/TheBlueBanner.vue
+++ src/components/TheBlueBanner.vue
@@ -1,6 +1,34 @@
 <script>
 export default {
 
+    data() {
+        return {
+            // bannerList: [
+            //     {
+
+            //         imgBanner: [
+            //             'buy-comics-digital-comics.',
+            //             'buy-comics-merchandise.',
+            //             'buy-comics-subscriptions.',
+            //             'buy-comics-shop-locator.',
+            //             'buy-dc-power-visa.',
+            //         ],
+            //         titleBanner: [
+                        
+            //             'digital comics',
+            //             'dc merchandise',
+            //             'subscription',
+            //             'comic shop locator',
+            //             'dc power visa',
+            //         ]
+                    
+                    
+            //     }
+            // ]
+            
+        }
+    }
+
 };
 </script>
 
@@ -10,29 +38,41 @@ export default {
         <div class="container">
             <div class="banner-main z-1">
                 <div>
-                    <a class="d-flex aligh-item-center" href="#"> 
-                        <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+                    <a class="d-flex align-items-center" href="#">
+                        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
                         <span>digital comics</span>
                     </a>
                 </div>
                 <div>
-                    <img src="../assets/img/buy-comics-merchandise.png" alt="dc-merchandise">
-                    <span>dc merchandise</span>
+                    <a class="d-flex align-items-center" href="#">
+
+                        <img src="../assets/img/buy-comics-merchandise.png" alt="dc-merchandise">
+                        <span>dc merchandise</span>
+                    </a>
 
                 </div>
                 <div>
-                    <img src="../assets/img/buy-comics-subscriptions.png" alt="subscription">
-                    <span>subscription</span>
+                    <a class="d-flex align-items-center" href="#">
+
+                        <img src="../assets/img/buy-comics-subscriptions.png" alt="subscription">
+                        <span>subscription</span>
+                    </a>
 
                 </div>
                 <div>
-                    <img src="../assets/img/buy-comics-shop-locator.png" alt="comic-shop">
-                    <span>comic shop locator</span>
+                    <a class="d-flex align-items-center" href="#">
+
+                        <img src="../assets/img/buy-comics-shop-locator.png" alt="comic-shop">
+                        <span>comic shop locator</span>
+                    </a>
 
                 </div>
                 <div>
-                    <img src="../assets/img/buy-dc-power-visa.svg" alt="dc-power" style="height: 50px;">
-                    <span>dc power visa</span>
+                    <a class="d-flex align-items-center" href="#">
+
+                        <img src="../assets/img/buy-dc-power-visa.svg" alt="dc-power" style="height: 50px;">
+                        <span>dc power visa</span>
+                    </a>
 
                 </div>
             </div>
@@ -44,7 +84,7 @@ export default {
 <style scoped lang="scss">
 @use "../styles/partials/variables" as *;
 
-.blue-banner{
+.blue-banner {
     font-family: $font-primary;
     background-color: $color-primary;
     padding-top: 2rem;
